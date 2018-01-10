91a89adc97adca5fd03e04b635c8081d128d44a2
Haricharan Konduri <hkonduri@ceredox.com>
2018-01-10 09:14:12 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req,res,next) => {
+    var now = new Date().toString();
+    var log= `${now}: ${req.method} ${req.url} IP:${req.connection.remoteAddress}`;
+    fs.appendFile('server.log', log +'\n', (err)=>{
+        if(err){
+            console.log('unable to append to server log');
+        }
+    })
+    next();
+});
+
+app.use((req,res,next)=>{
+
+    res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIf',(stext) => {
+    return stext.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+
+
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Welcome to the Home Page'
+    });
+
+
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        someText: 'yolo tolo bolo'
+    });
+});
+
+
+
+app.get('/bad', (req, res) => {
+
+
+    res.send({
+        errorMessage: 'unable to handle request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('server is upon port 3000');
+});
